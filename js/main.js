67832f7781386a0dca2d88131b2dd82a855f7d37
Philip Yoo <philipsdyoo@msn.com>
2015-11-07 02:34:35 -0500

    Lots of frontend work and added an About page

--- js/main.js
+++ js/main.js
@@ -33,18 +33,21 @@ $(document).ready(function() {
 				data.push("FREE");
 			}
 		}
-		var findata = [];
+		var fin_data = [];
 		for (var i = 0; i < 25; i++) {
 			var datum = "";
 			if (i != 12) {
 				var rand = Math.floor(Math.random() * data.length);
 				datum = data[rand];
-				findata.push(datum);
+				if (datum == "") {
+					datum = "FREE";
+				}
+				fin_data.push(datum);
 				data.splice(rand, 1);
 			}
 			else {
 				datum = "FREE";
-				findata.push("FREE");
+				fin_data.push("FREE");
 			}
 			$("#board" + i).text(datum);
 		}
@@ -59,11 +62,14 @@ $(document).ready(function() {
 				data.push("FREE");
 			}
 		}
-		var findata = [];
+		var fin_data = [];
 		for (var i = 0; i < 25; i++) {
 			var rand = Math.floor(Math.random() * data.length);
 			var datum = data[rand];
-			findata.push(datum);
+			if (datum == "") {
+				datum = "FREE";
+			}
+			fin_data.push(datum);
 			data.splice(rand, 1);
 			$("#board" + i).text(datum);
 		}
@@ -73,6 +79,9 @@ $(document).ready(function() {
 	$("#grid-create").click(function() {
 		for (var i = 0; i < 25; i++) {
 			var datum = $("#setup" + i).val();
+			if (datum == "") {
+				datum = "FREE";
+			}
 			$("#board" + i).text(datum);
 		}
 		$("#setup").hide();
